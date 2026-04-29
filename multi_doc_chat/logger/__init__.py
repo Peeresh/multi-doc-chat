a35788bad455b9b5155ae457912e7f6826e7582f
Peeresh <ep22btech11011@iith.ac.in>
2026-04-29 22:45:47 +0530

    Fixing Logger error

--- multi_doc_chat/logger/__init__.py
+++ multi_doc_chat/logger/__init__.py
@@ -1,3 +1,7 @@
-from .custom_logger import CustomLogger
+from .custom_logger import CustomLogger as _CustomLogger
+try:
+    from .custom_logger import CustomLogger
+except Exception:
+    CustomLogger = _CustomLogger
 
 GLOBAL_LOGGER = CustomLogger().get_logger()
